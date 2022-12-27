f1b13b45d12fed6d07a9486e1f7f2ae5ee808ccc
Chen <105437124+ChenFan-Github@users.noreply.github.com>
2022-12-27 12:30:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from charting import InitCharts, PlotPerformanceChart, PlotPosConcentrationChart
 class TradingBot(QCAlgorithm):
     def Initialize(self):
         # Setup time spam and cash
-        self.SetStartDate(2021, 9, 1)
+        self.SetStartDate(2021, 11, 1)
         self.SetEndDate(2022, 12, 27)
         self.SetCash(100000)
         
